78ca6613e50ca05b7039c78ec88a27ce973521b3
maria_paula <mp.ramos@uniandes.edu.co>
2019-11-19 18:43:18 +0000

    tercera grafica

---
+++ varios_tiempos.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+data = np.loadtxt('grilla.dat')
+x = np.linspace(-1,1,data.shape[1])
+
+tiempos = [0,110,220,330,440,560,670,780]
+for i in tiempos:
+    plt.plot(x,data[i,:], label = i)
+plt.legend()
+plt.savefig('grafica_X_cero.png')
