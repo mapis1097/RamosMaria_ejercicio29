3fa8bb4724d823a075421952d78807d180591786
maria_paula <mp.ramos@uniandes.edu.co>
2019-11-19 18:11:32 +0000

    grafica

--- grafica29.py
+++ grafica29.py
@@ -3,16 +3,15 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
+data = np.loadtxt('grilla.dat')
+x = np.linspace(-1,1,data.shape[1])
+t = np.linspace(0,1,data.shape[0])
 
-data = np.loadtxt("grilla.dat")
-x = np.linspace(0.0, 1.0, 30)
-t = np.linspace(0.0, 1.0, 30)
-X, T = np.meshgrid(x,t)
+X,T = np.meshgrid(x,t)
 
-figura = plt.figure(figsize=(13,5))
-axis = fig.add_subplot(121, projection="3d")
-superficie = axis.plot_superficie(X, T, data, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-plt.xlabel("Posicion (m)")
-plt.ylabel("Tiempo (s)")
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.savefig("gradiente.png")
+figura = plt.figure()
+axis = figura.gca(projection='3d')
+surf = axis.plot_surface(X,T,data)
+plt.xlabel('Espacio')
+plt.ylabel('Tiempo')
+plt.savefig('grafica.png')
